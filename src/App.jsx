ffe16c9b43e3938ba842a7df4f39a944b8672b66
Thanh Duong <thanhduong20066@gmail.com>
2024-11-06 11:32:08 +0700

    Change content about me, picture, remove component Experience

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import About from "./components/About"
 import Technologies from "./components/Technologies"
-import Experience from "./components/Experience"
 import Projects from "./components/Projects"
 import Contact from "./components/Contact"
 
@@ -27,7 +26,6 @@ const App = () => {
         <Hero />
         <About />
         <Technologies />
-        <Experience />
         <Projects />
         <Contact />
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from '../assets/kevinRushLogo.png';
+import logo from '../assets/logo_name.png';
 import { FaLinkedin } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 import { FaFacebook } from 'react-icons/fa';
@@ -9,13 +9,14 @@ const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img className="mx-2 w-10" src={logo} alt="" />
+                <img className="mx-2 w-14" src={logo} alt="" />
             </div>
             <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-                <FaLinkedin />
-                <FaGithub />
-                <FaFacebook />
-                <FaInstagram />
+                <a href="https://www.linkedin.com/in/tranthanhduong03/"><FaLinkedin /></a>
+                <a href="https://github.com/DuongDADEV"><FaGithub /></a>
+                <a href="https://www.facebook.com/profile.php?id=100038824158900"><FaFacebook /></a>
+                <a href=""><FaInstagram /></a>
+                
 
             </div>
         </nav>
